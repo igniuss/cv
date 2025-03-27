697df477dc48569d6c498775160f0db361d9533a
Sean L. <slava_be@hotmail.com>
2025-03-27 23:20:49 +0100

    extracted box as component

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,7 @@
   import gravitar from "./assets/gravitar.jpg";
   import qatool from "./assets/screenshots/qa-tool.png";
   import { MailPlus, PhoneCall } from "lucide-svelte";
+  import Box from "./lib/Box.svelte";
 
   const tbwGallery = Object.values(
     import.meta.glob(
@@ -30,6 +31,10 @@
     "NixOS",
     "Linux",
     "Optimization Quirks",
+    "Rust 🦀",
+    "Unity3D",
+    "Murder (the ECS engine)",
+    "Music"
   ];
 
   function getInterest() {
@@ -93,126 +98,97 @@
     />
 
     <Divider />
-    <div class="flex flex-col gap-4">
-      <p class="text-3xl">Work</p>
-      <div class="flex-col flex gap-2 border rounded-xl p-4">
-        <span class="flex align-middle justify-center">
-          <p class="text-2xl absolute -translate-y-9 bg-white px-5">
-            Toca Boca World
-          </p>
-        </span>
-        <p>Award Winning title on iOS and Android</p>
-        <p class="text-3xl"></p>
-        <p class="text-2xl">Outfit Maker</p>
-        <p>Player Facing outfit designer tool</p>
-        <p class="text-2xl">SVG Renderer</p>
-        <p>
-          Developer Facing system that creates 3D Meshes out of SVG Files to be
-          rendered on the GPU
-        </p>
-        <p class="text-2xl">External QA Tool</p>
-        <p>
-          Developer Facing system for debugging builds on external devices,
-          built with Svelte
-        </p>
-        <details>
-          <summary class="text-xs">QA Tool - Screenshots</summary>
-          <div class="w-full">
-            <img src={qatool} alt="" class="rounded-3xl object-contain" />
-          </div>
-        </details>
+    <Box title="Toca Boca World">
+      <p>Award Winning title on iOS and Android</p>
+      <p class="text-3xl"></p>
+      <p class="text-2xl">Outfit Maker</p>
+      <p>Player Facing outfit designer tool</p>
+      <p class="text-2xl">SVG Renderer</p>
+      <p>
+        Developer Facing system that creates 3D Meshes out of SVG Files to be
+        rendered on the GPU
+      </p>
+      <p class="text-2xl">External QA Tool</p>
+      <p>
+        Developer Facing system for debugging builds on external devices, built
+        with Svelte
+      </p>
+      <details>
+        <summary class="text-xs">QA Tool - Screenshots</summary>
+        <div class="w-full">
+          <img src={qatool} alt="" class="rounded-3xl object-contain" />
+        </div>
+      </details>
 
-        <details>
-          <summary class="text-xs">Game Screenshots</summary>
-          <div class="grid grid-cols-2 md:grid-cols-3 gap-4 m-4">
-            {#each tbwGallery as img}
-              <img src={img} class="object-contain rounded-xl" alt={img} />
-            {/each}
-          </div>
-        </details>
-      </div>
+      <details>
+        <summary class="text-xs">Game Screenshots</summary>
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-4 m-4">
+          {#each tbwGallery as img}
+            <img src={img} class="object-contain rounded-xl" alt={img} />
+          {/each}
+        </div>
+      </details>
+    </Box>
+    <br />
+    <Box title="Homebrew - Patent Unknown">
+      <p class="italic">Roles:</p>
+      <ul class="list-disc space-y-1 list-inside">
+        <li>Programmer</li>
+        <li>Community Manager</li>
+        <li>Modding API</li>
+        <li>Multiplayer Services</li>
+        <li>Website Maintainer</li>
+        <li>Wiki Maintainer</li>
+      </ul>
+      <Divider />
+      <p class="italic">Languages | Services</p>
+      <ul class="list-disc space-y-1 list-inside">
+        <li>C#</li>
+        <li>ASP.Net</li>
+        <li>SteamWorks</li>
+        <li>C++</li>
+        <li>Html | CSS | Javascript</li>
+        <li>PHP</li>
+      </ul>
+      <Divider />
+      <p class="italic text-lg">Early Access title on Steam</p>
+
+      <p>
+        Unleash your creativity and craft anything from flying cars to
+        jet-powered toilets.Fair warning: there's a learning curve. But once
+        you've got it, the sky's the limit for your creations!While active
+        development is paused, we focus preserving the game and keeping it
+        accessible.
+      </p>
       <br />
-      <div class="flex-col flex gap-2 border rounded-xl p-4">
-        <span class="flex align-middle justify-center">
-          <p
-            class="text-2xl absolute -translate-y-9 bg-white px-5 rounded-xl whitespace-nowrap"
-          >
-            Homebrew - Patent Unkown
-          </p>
-        </span>
+      <p>
+        Every vehicle showcased in the screenshots is a testament to our
+        community's ingenuity. Homebrew is crafted with passion by a small,
+        dedicated team and while we've poured our heart and soul into this
+        project it has its quirks. The learning curve is steep, and there may be
+        imperfections, but if you crave a challenge and a platform where
+        creativity knows no bounds, Homebrew awaits.
+      </p>
 
-        <p class="italic">Roles:</p>
-        <ul class="list-disc space-y-1 list-inside">
-          <li>Programmer</li>
-          <li>Community Manager</li>
-          <li>Modding API</li>
-          <li>Multiplayer Services</li>
-          <li>Website Maintainer</li>
-          <li>Wiki Maintainer</li>
-        </ul>
-        <Divider />
-        <p class="italic">Languages | Services</p>
-        <ul class="list-disc space-y-1 list-inside">
-          <li>C#</li>
-          <li>ASP.Net</li>
-          <li>SteamWorks</li>
-          <li>C++</li>
-          <li>Html | CSS | Javascript</li>
-          <li>PHP</li>
-        </ul>
-        <Divider />
-        <p class="italic text-lg">Early Access title on Steam</p>
-
-        <p>
-          Unleash your creativity and craft anything from flying cars to
-          jet-powered toilets.Fair warning: there's a learning curve. But once
-          you've got it, the sky's the limit for your creations!While active
-          development is paused, we focus preserving the game and keeping it
-          accessible.
-        </p>
-        <br />
-        <p>
-          Every vehicle showcased in the screenshots is a testament to our
-          community's ingenuity. Homebrew is crafted with passion by a small,
-          dedicated team and while we've poured our heart and soul into this
-          project it has its quirks. The learning curve is steep, and there may
-          be imperfections, but if you crave a challenge and a platform where
-          creativity knows no bounds, Homebrew awaits.
-        </p>
-
-        <details>
-          <summary class="text-xs">Game Screenshots</summary>
-          <div class="grid grid-cols-2 md:grid-cols-3 gap-4 m-4">
-            {#each hbGallery as img}
-              <img src={img} class="object-contain rounded-xl" alt={img} />
-            {/each}
-          </div>
-        </details>
-      </div>
-      <br />
-      <div class="flex-col flex gap-2 border rounded-xl p-4">
-        <span class="flex align-middle justify-center">
-          <p class="text-2xl absolute -translate-y-9 bg-white px-5 rounded-xl">
-            Projects under NDA
-          </p>
-        </span>
-        <p>4 titles for Escape Rooms in Belgium</p>
-        <p>2 multiplayer titles for VR Arcade in Belgium</p>
-        <p>Booking system written in C# (ASP.Net) with Lua modules</p>
-        <p>
-          Cloud Orchestrator system written in C# (ASP.Net) with Lua modules
-        </p>
-      </div>
-    </div>
+      <details>
+        <summary class="text-xs">Game Screenshots</summary>
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-4 m-4">
+          {#each hbGallery as img}
+            <img src={img} class="object-contain rounded-xl" alt={img} />
+          {/each}
+        </div>
+      </details>
+    </Box>
+    <br />
+    <Box title="Projects under NDA">
+      <p>4 titles for Escape Rooms in Belgium</p>
+      <p>2 multiplayer titles for VR Arcade in Belgium</p>
+      <p>Booking system written in C# (ASP.Net) with Lua modules</p>
+      <p>Cloud Orchestrator system written in C# (ASP.Net) with Lua modules</p>
+    </Box>
     <Divider />
     <br />
-    <div class="border rounded-xl p-4">
-      <span class="flex align-middle justify-center">
-        <p class="text-2xl absolute -translate-y-9 bg-white px-5 rounded-xl">
-          Other Information
-        </p>
-      </span>
-      <br />
+    <Box title="Other Information">
       <p>
         During my time in the industry, I have done many presentations, in-house
         for fellow developers, and a handful for external companies
@@ -223,8 +199,12 @@
         topics
       </p>
       <br />
+      <p class="text-2xl">Presentations</p>
       <Presentations />
-    </div>
+
+      <br>
+      <Divider />
+    </Box>
   </div>
 
   <div class="flex py-4">
---
+++ src/lib/Box.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+let { title, children } = $props();
+</script>
+
+<div class="border rounded-xl p-4">
+  <span class="flex align-middle justify-center">
+    <p class="text-2xl absolute -translate-y-8 bg-white px-5 rounded-xl">
+      {title}
+    </p>
+  </span>
+  <br />
+  {@render children?.()}
+</div>
